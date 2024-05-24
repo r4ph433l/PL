aa34e7b2b456cbf39d7564d684fed580aee956f8
r4ph433l <70282135+r4ph433l@users.noreply.github.com>
2024-05-24 22:21:11 +0200

    Create my_std.scm

---
+++ my_std.scm
@@ -0,0 +1,28 @@
+define (length items)
+        (if (null? items) 0
+                (+ 1 (length (cdr items)))
+        )
+)
+
+(define (countin lst e)
+        (cond
+                ((null? lst) 0)
+                ((equal? (car lst) e) (+ 1 (countin (cdr lst) e)))
+                (else (countin (cdr lst) e))
+        )
+)
+
+(define (erase lst e)
+        (cond
+                ((null? lst) ())
+                ((equal? (car lst) e) (erase(cdr lst) e))
+                (else (cons (car lst)(erase (cdr lst) e)))
+        )
+)
+
+(define (at lst i)
+        (cond
+                ((equal? i 0) (car lst))
+                (else (at (cdr lst) (- i 1)))
+        )
+)
